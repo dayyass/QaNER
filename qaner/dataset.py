fix train pipeline and add metrics calculation

qaner/dataset.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 import torch
 import transformers
@@ -51,15 +51,16 @@ class Dataset(torch.utils.data.Dataset):
                         answer_list.append(span)
 
                 if len(answer_list) == 0:
+                    empty_span = Span(
+                        token="",
+                        label="O",
+                        start_context_char_pos=0,
+                        end_context_char_pos=0,
+                    )
                     instance = Instance(
                         context=sentence,
                         question=question_prompt,
-                        answer=Span(
-                            token="",
-                            label="O",
-                            start_pos=0,
-                            end_pos=0,
-                        ),
+                        answer=empty_span,
                     )
                     dataset.append(instance)
                 else:
@@ -90,21 +91,82 @@ class Collator:
 
         context_list = []
         question_list = []
-        start_pos_list, end_pos_list = [], []
+        start_end_context_char_pos_list = []
 
         for instance in batch:
             context_list.append(instance.context)
             question_list.append(instance.question)
-            start_pos_list.append(instance.answer.start_pos)
-            end_pos_list.append(instance.answer.end_pos)
+            start_end_context_char_pos_list.append(
+                [
+                    instance.answer.start_context_char_pos,
+                    instance.answer.end_context_char_pos,
+                ]
+            )
 
         tokenized_batch = self.tokenizer(
             question_list, context_list, **self.tokenizer_kwargs
         )
-        tokenized_batch["start_positions"] = torch.LongTensor(start_pos_list)
-        tokenized_batch["end_positions"] = torch.LongTensor(end_pos_list)
+
+        offset_mapping_batch = tokenized_batch["offset_mapping"].numpy().tolist()
+
+        assert len(offset_mapping_batch) == len(start_end_context_char_pos_list)
+
+        # convert char boudaries to token boudaries in batch
+        start_token_pos_list, end_token_pos_list = [], []
+        for offset_mapping, start_end_context_char_pos in zip(
+            offset_mapping_batch, start_end_context_char_pos_list
+        ):
+            if start_end_context_char_pos == [0, 0]:
+                start_token_pos_list.append(0)
+                end_token_pos_list.append(0)
+            else:
+                (
+                    start_token_pos,
+                    end_token_pos,
+                ) = self.convert_char_boudaries_to_token_boudaries(
+                    offset_mapping=offset_mapping,
+                    start_end_context_char_pos=start_end_context_char_pos,
+                )
+                start_token_pos_list.append(start_token_pos)
+                end_token_pos_list.append(end_token_pos)
+
+        tokenized_batch["start_positions"] = torch.LongTensor(start_token_pos_list)
+        tokenized_batch["end_positions"] = torch.LongTensor(end_token_pos_list)
 
         # additional
         tokenized_batch["instances"] = batch
 
         return tokenized_batch
+
+    # TODO: add tests
+    @staticmethod
+    def convert_char_boudaries_to_token_boudaries(
+        offset_mapping: List[List[int]],
+        start_end_context_char_pos: List[int],
+    ) -> Tuple[int, int]:
+
+        start_context_char_pos, end_context_char_pos = start_end_context_char_pos
+        assert end_context_char_pos >= start_context_char_pos
+
+        done = False
+        special_tokens_cnt = 0
+        for i, token_boudaries in enumerate(offset_mapping):
+
+            if token_boudaries == [0, 0]:
+                special_tokens_cnt += 1
+                continue
+
+            if special_tokens_cnt == 2:
+                start_token_pos, end_token_pos = token_boudaries
+
+                if start_token_pos == start_context_char_pos:
+                    res_start_token_pos = i
+
+                if end_token_pos == end_context_char_pos:
+                    res_end_token_pos = i  # inclusive
+                    done = True
+                    break
+
+        assert done
+
+        return res_start_token_pos, res_end_token_pos

qaner/train_utils.py
@@ -1,16 +1,19 @@
+from collections import defaultdict
+
 import numpy as np
 import torch
+from data_utils import Span
 from inference_utils import get_top_valid_spans
+from metrics import compute_metrics
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+from transformers import AutoModelForQuestionAnswering
 
 
 # TODO: add metrics calculation
 def train(
     n_epochs: int,
     model: AutoModelForQuestionAnswering,
-    tokenizer: AutoTokenizer,
     train_dataloader: torch.utils.data.DataLoader,
     test_dataloader: torch.utils.data.DataLoader,
     optimizer: torch.optim.Optimizer,
@@ -23,7 +26,6 @@ def train(
     Args:
         n_epochs (int): number of epochs to train.
         model (AutoModelForQuestionAnswering): model.
-        tokenizer (AutoTokenizer): tokenizer.
         train_dataloader (torch.utils.data.DataLoader): train_dataloader.
         test_dataloader (torch.utils.data.DataLoader): test_dataloader.
         optimizer (torch.optim.Optimizer): optimizer.
@@ -37,7 +39,6 @@ def train(
 
         train_epoch(
             model=model,
-            tokenizer=tokenizer,
             dataloader=train_dataloader,
             optimizer=optimizer,
             writer=writer,
@@ -46,7 +47,6 @@ def train(
         )
         evaluate_epoch(
             model=model,
-            tokenizer=tokenizer,
             dataloader=test_dataloader,
             writer=writer,
             device=device,
@@ -56,7 +56,6 @@ def train(
 
 def train_epoch(
     model: AutoModelForQuestionAnswering,
-    tokenizer: AutoTokenizer,
     dataloader: torch.utils.data.DataLoader,
     optimizer: torch.optim.Optimizer,
     writer: SummaryWriter,
@@ -68,7 +67,6 @@ def train_epoch(
 
     Args:
         model (AutoModelForQuestionAnswering): QA model.
-        tokenizer (AutoTokenizer): tokenizer.
         dataloader (torch.utils.data.DataLoader): dataloader.
         optimizer (torch.optim.Optimizer): optimizer.
         writer (SummaryWriter): tensorboard writer.
@@ -79,6 +77,7 @@ def train_epoch(
     model.train()
 
     epoch_loss = []
+    batch_metrics_list = defaultdict(list)
 
     for i, inputs in tqdm(
         enumerate(dataloader),
@@ -105,24 +104,53 @@ def train_epoch(
         with torch.no_grad():
             model.eval()
             outputs_inference = model(**inputs)
-            top_1_spans_batch = get_top_valid_spans(  # noqa: F841
-                tokenizer=tokenizer,
-                inputs=inputs,
-                outputs=outputs_inference,
-                offset_mapping_batch=offset_mapping_batch,
-                instances_batch=instances_batch,
-                n_best_size=1,
-                max_answer_length=100,
+            model.train()
+
+        spans_pred_batch_top_1 = get_top_valid_spans(
+            inputs=inputs,
+            outputs=outputs_inference,
+            offset_mapping_batch=offset_mapping_batch,
+            instances_batch=instances_batch,
+            n_best_size=1,
+            max_answer_length=100,
+        )
+
+        # TODO: maybe move into get_top_valid_spans
+        for i in range(len(spans_pred_batch_top_1)):
+            if not spans_pred_batch_top_1[i]:
+                empty_span = Span(
+                    token="",
+                    label="O",
+                    start_context_char_pos=0,
+                    end_context_char_pos=0,
+                )
+                spans_pred_batch_top_1[i] = [empty_span]
+
+        spans_true_batch = [instance.answer for instance in instances_batch]
+
+        batch_metrics = compute_metrics(
+            spans_true_batch=spans_true_batch,
+            spans_pred_batch_top_1=spans_pred_batch_top_1,
+        )
+
+        for metric_name, metric_value in batch_metrics.items():
+            batch_metrics_list[metric_name].append(metric_value)
+            writer.add_scalar(
+                f"batch {metric_name} / train",
+                metric_value,
+                epoch * len(dataloader) + i,
             )
 
     avg_loss = np.mean(epoch_loss)
     print(f"Train loss: {avg_loss}\n")
     writer.add_scalar("loss / train", avg_loss, epoch)
 
+    for metric_name, metric_value_list in batch_metrics_list.items():
+        writer.add_scalar(f"{metric_name} / train", np.mean(metric_value_list), epoch)
+
 
 def evaluate_epoch(
     model: AutoModelForQuestionAnswering,
-    tokenizer: AutoTokenizer,
     dataloader: torch.utils.data.DataLoader,
     writer: SummaryWriter,
     device: torch.device,
@@ -133,7 +161,6 @@ def evaluate_epoch(
 
     Args:
         model (AutoModelForQuestionAnswering): model.
-        tokenizer (AutoTokenizer): tokenizer.
         dataloader (torch.utils.data.DataLoader): dataloader.
         writer (SummaryWriter): tensorboard writer.
         device (torch.device): cpu or cuda.
@@ -152,8 +179,10 @@ def evaluate_epoch(
             desc="loop over test batches",
         ):
 
+            # instances_batch = inputs.pop("instances")
+
             inputs = inputs.to(device)
-            # offset_mapping_batch = inputs.pop("offset_mapping")  # TODO
+            # offset_mapping_batch = inputs.pop("offset_mapping")
 
             outputs = model(**inputs)
             loss = outputs.loss

tests/test_dataset.py
@@ -24,9 +24,9 @@ def validate_spans(
     ):
         for span in labels:
             token = span.token
-            start_pos = span.start_pos
-            end_pos = span.end_pos
-            assert token == sentence[start_pos:end_pos]
+            start_context_char_pos = span.start_context_char_pos
+            end_context_char_pos = span.end_context_char_pos
+            assert token == sentence[start_context_char_pos:end_context_char_pos]
     return True
 
 
@@ -40,6 +40,7 @@ tokenizer_kwargs = {  # TODO: validate it
     "truncation": "only_second",
     "padding": True,
     "return_tensors": "pt",
+    "return_offsets_mapping": True,
 }
 
 
@@ -81,8 +82,8 @@ class TestDataset(unittest.TestCase):
             answer=Span(
                 token="",
                 label="O",
-                start_pos=0,
-                end_pos=0,
+                start_context_char_pos=0,
+                end_context_char_pos=0,
             ),
         )
 
@@ -95,8 +96,8 @@ class TestDataset(unittest.TestCase):
             answer=Span(
                 token="EU",
                 label="ORG",
-                start_pos=0,
-                end_pos=2,
+                start_context_char_pos=0,
+                end_context_char_pos=2,
             ),
         )
 
@@ -109,8 +110,8 @@ class TestDataset(unittest.TestCase):
             answer=Span(
                 token="German",
                 label="MISC",
-                start_pos=11,
-                end_pos=17,
+                start_context_char_pos=11,
+                end_context_char_pos=17,
             ),
         )
 
@@ -124,8 +125,8 @@ class TestDataset(unittest.TestCase):
             answer=Span(
                 token="European Commission",
                 label="ORG",
-                start_pos=4,
-                end_pos=23,
+                start_context_char_pos=4,
+                end_context_char_pos=23,
             ),
         )
 
@@ -139,8 +140,8 @@ class TestDataset(unittest.TestCase):
             answer=Span(
                 token="Nikolaus van der Pas",
                 label="PER",
-                start_pos=123,
-                end_pos=143,
+                start_context_char_pos=123,
+                end_context_char_pos=143,
             ),
         )
 
@@ -222,15 +223,82 @@ class TestCollator(unittest.TestCase):
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             ]
         )
+        batch_true["offset_mapping"] = torch.tensor(
+            [
+                [
+                    [0, 0],
+                    [0, 4],
+                    [5, 7],
+                    [8, 11],
+                    [12, 20],
+                    [20, 21],
+                    [0, 0],
+                    [0, 2],
+                    [3, 10],
+                    [11, 17],
+                    [18, 22],
+                    [23, 25],
+                    [26, 33],
+                    [34, 41],
+                    [42, 46],
+                    [47, 48],
+                    [0, 0],
+                ],
+                [
+                    [0, 0],
+                    [0, 4],
+                    [5, 7],
+                    [8, 11],
+                    [12, 18],
+                    [18, 19],
+                    [0, 0],
+                    [0, 2],
+                    [3, 10],
+                    [11, 17],
+                    [18, 22],
+                    [23, 25],
+                    [26, 33],
+                    [34, 41],
+                    [42, 46],
+                    [47, 48],
+                    [0, 0],
+                ],
+            ]
+        )
         batch_true["start_positions"] = torch.tensor([0, 0])
         batch_true["end_positions"] = torch.tensor([0, 0])
+        batch_true["instances"] = [
+            Instance(
+                context="EU rejects German call to boycott British lamb .",
+                question="What is the location?",
+                answer=Span(
+                    token="",
+                    label="O",
+                    start_context_char_pos=0,
+                    end_context_char_pos=0,
+                ),
+            ),
+            Instance(
+                context="EU rejects German call to boycott British lamb .",
+                question="What is the person?",
+                answer=Span(
+                    token="",
+                    label="O",
+                    start_context_char_pos=0,
+                    end_context_char_pos=0,
+                ),
+            ),
+        ]
 
         batch_pred = next(iter(dataloader))
 
         for key in batch_true:
-            self.assertTrue(
-                torch.allclose(batch_true[key], batch_pred[key]),
-            )
+            if key == "instances":
+                self.assertListEqual(batch_true[key], batch_pred[key])
+            else:
+                self.assertTrue(
+                    torch.allclose(batch_true[key], batch_pred[key]),
+                )
 
 
 if __name__ == "__main__":